b810d27852328f68057b9f122112408b23f41bfb
dmoyon <dennysmoyon@gmail.com>
2018-06-07 12:55:56 -0500

    socket para escuchar cambios en registro en db para acciones en publicaciones.

--- src/lukask_project/lukask_app/bindings.py
+++ src/lukask_project/lukask_app/bindings.py
@@ -2,8 +2,10 @@ from channels_api.bindings import ResourceBinding
 
 from .models import Multimedia
 from .models import Publication
+from .models import ActionPublication
 from .serializers import PublicationSerializer
 from .serializers import MultimediaSerializer
+from .serializers import ActionSerializer
 from channels_api.permissions import IsAuthenticated
 
 class PublicationBinding(ResourceBinding):
@@ -19,4 +21,9 @@ class MultimediaBinding(ResourceBinding):
     serializer_class = MultimediaSerializer
     queryset = Multimedia.objects.all()
 
+class ActionPublicationBinding(ResourceBinding):
+    model = ActionPublication
+    stream = "comments"
+    serializer_class = ActionSerializer
+    queryset = ActionPublication.objects.all()
 
--- src/lukask_project/lukask_project/routing.py
+++ src/lukask_project/lukask_project/routing.py
@@ -3,12 +3,14 @@ from channels.generic.websockets import WebsocketDemultiplexer
 
 from lukask_app.bindings import PublicationBinding
 from lukask_app.bindings import MultimediaBinding
+from lukask_app.bindings import ActionPublicationBinding
 
 
 class ChannelApiAppDemultiplexer(WebsocketDemultiplexer):
     consumers = {
         'publication': PublicationBinding.consumer,
-        'multimedia' : MultimediaBinding.consumer
+        'multimedia' : MultimediaBinding.consumer,
+        'comments'   : ActionPublicationBinding.consumer
     }
 
 channel_routing = [
