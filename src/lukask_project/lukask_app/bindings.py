31487f877c76836b50a99704b832f95994bbc20b
dennysmoyon <dmoyon@horizon-ts.com>
2018-09-21 19:49:13 -0500

    create binding for  custom create  of publications.

--- src/lukask_project/lukask_app/bindings.py
+++ src/lukask_project/lukask_app/bindings.py
@@ -1,4 +1,7 @@
+from django.core import serializers
+
 from channels_api.bindings import ResourceBinding
+from channels_api.decorators import detail_action, list_action
 
 from .models import Multimedia
 from .models import Publication
@@ -8,13 +11,25 @@ from .serializers import PublicationSerializer
 from .serializers import MultimediaSerializer
 from .serializers import ActionSerializer
 from .serializers import NotificationReceivedSerializer
-from channels_api.permissions import IsAuthenticated
 
 class PublicationBinding(ResourceBinding):
     model = Publication
     stream = "publication"
     serializer_class = PublicationSerializer
     queryset = Publication.objects.all()
+
+    @detail_action()
+    def custom_create(self, pk, data=None, **kwargs):
+        publication = Publication.objects.get(id_publication = pk)
+        serializer_p = PublicationSerializer(publication)
+        result =  serializer_p.data
+        print ("result....", result)
+        return result, 200
+
+    """@list_action()
+    def report(self, data=None, **kwargs):
+        report = self.get_queryset()
+        return report, 200"""
 
 
 class MultimediaBinding(ResourceBinding):
