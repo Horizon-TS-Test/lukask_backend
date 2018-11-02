0936da30d8619498bb6169f78b67b2d1f730bc79
dennysmoyon <dmoyon@horizon-ts.com>
2018-11-02 12:26:00 -0500

    change config daphne server.

--- src/lukask_project/lukask_project/asgi.py
+++ src/lukask_project/lukask_project/asgi.py
@@ -1,6 +1,19 @@
-import os
+"""import os
 from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lukask_project.settings")
 
-channel_layer = get_channel_layer()
+channel_layer = get_channel_layer()"""
+
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+application = get_default_application()
